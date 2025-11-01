6a06ffb38a6d425c09f390e7ba1edc0e900a20b9
00400622-hub <00400622@uca.edu.sv>
2025-11-01 16:16:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ if seleccion == "Ventas":
     mostrar_venta()
 elif seleccion == "Otra opción":
     st.write("Has seleccionado otra opción.")  # Aquí podrías agregar el contenido de otras opciones
-
-    # Si la sesión está iniciada, mostrar el contenido de ventas
-    mostrar_venta()
+    
 else:
     # Si la sesión no está iniciada, mostrar el login
     login()
